9263a5838e396eec4adbf3e83646f1fbfcbb9115
raugh135 <55518299+raugh135@users.noreply.github.com>
2019-11-07 10:17:50 +0800

    B1040429-11-07

---
+++ UnityProject/Assets/Ball.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    [Range(0 , 1000) , Header("速度")]
+    public int speed = 50;
+
+    [Range(0, 1000) , Header("跳躍")]
+    public int jump = 100;
+
+    [Header("是否在地上")]
+    public bool isGround = true;
+
+    private Rigidbody r3d;
+
+	// Use this for initialization
+	void Start () {
+
+        r3d = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGround = true;
+    }
+
+    private void FixedUpdate()
+    {
+        Jump();
+        Walk();
+    }
+
+    /// <summary>
+    /// 走路
+    /// </summary>
+    private void Walk()
+    {
+        r3d.AddForce(new Vector3(speed * Input.GetAxis("Horizontal"), 0 , speed * Input.GetAxis("Vertical")));
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            r3d.AddForce(new Vector3(0, jump, 0));
+        }
+        }
+    }
+
---
+++ UnityProject/Assets/Robot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour {
+    [Header("對話")]
+    public string Say = "OMG";
+    public string SaySpeed = "0.5";
+
+    [Header("任務")]
+    public bool Complete;
+    public string BateryFinish = "5";
+    public string Batery = "0";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
